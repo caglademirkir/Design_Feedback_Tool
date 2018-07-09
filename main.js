9575f4e8af38a27c99a70849c41c03a5f6dc7230
U130709023 <caglademirkr@gmail.com>
2018-07-09 16:10:31 +0300

    Yuvaklak objenin konum ve içindeki sayi artisi düzeltildi

--- main.js
+++ main.js
@@ -3,27 +3,34 @@ function readURL(input) {
         var reader = new FileReader();
         reader.onload = function (e) {
             $('#blah')
-                .attr('src', e.target.result)
-            
+                .attr('src', e.target.result)            
         };
         reader.readAsDataURL(input.files[0]);
         
     }
-    var circle = document.querySelector(".numberCircle");
+
     var container = document.querySelector(".img");
     container.addEventListener("click",getClickPosition, false);
 
     function getClickPosition(event){
-        var pos = $(this).position();
-        var x = event.clientX - (circle.offsetWidth / 2);
-        var y = event.clientY - (circle.offsetHeight / 2);
+        for(var i=0; i < 5; i++){
+            var circle = document.querySelector("#numberCircle");
+            var pos = $(this).position();
+            var x = event.clientX - (circle.offsetWidth / 2);
+            var y = event.clientY - (circle.offsetHeight / 2);
+            var translate3dValue = "translate3d(" + x +"px," + y + "px,0)";
+            circle.style.transform = translate3dValue;
 
-        var translate3dValue = "translate3d(" + x +"px," + y + "px,0)";
-        circle.style.transform = translate3dValue;
-
-        $(".numberCircle").css({
-            top : (pos.top + 3  ) + "px"
-        }).show();
+            $("#numberCircle").css({
+                top : (pos.top + 3  ) + "px"
+            }).show();
+            
+            var newScore = document.getElementById("numberCircle").innerHTML;
+            var value = parseInt(newScore,10) + 1;
+            document.getElementById("numberCircle").innerHTML = value;
+            
+            break;
+        }
     }
 }
 
