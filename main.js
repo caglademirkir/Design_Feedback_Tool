cccf47db69e16a024b9a76f03eb5e7099b43001b
U130709023 <caglademirkr@gmail.com>
2018-07-10 16:33:15 +0300

    errors corrected

--- main.js
+++ main.js
@@ -3,34 +3,44 @@ function readURL(input) {
         var reader = new FileReader();
         reader.onload = function (e) {
             $('#blah')
-                .attr('src', e.target.result)            
+                .attr('src', e.target.result)
         };
         reader.readAsDataURL(input.files[0]);
-        
+
     }
-
     var container = document.querySelector(".img");
-    container.addEventListener("click",getClickPosition, false);
+    container.addEventListener("click", getClickPosition, false);
+}
 
-    function getClickPosition(event){
-        for(var i=0; i < 5; i++){
-            var circle = document.querySelector("#numberCircle");
-            var pos = $(this).position();
-            var x = event.clientX - (circle.offsetWidth / 2);
-            var y = event.clientY - (circle.offsetHeight / 2);
-            var translate3dValue = "translate3d(" + x +"px," + y + "px,0)";
-            circle.style.transform = translate3dValue;
+function getClickPosition(event) {
+    var pos = $(this).position();
+    var newScore = $('#circleContent').children().last()[0].innerHTML;
+    var value = parseInt(newScore, 10) + 1;
+    var newDivId = "numberCircle" + value;
 
-            $("#numberCircle").css({
-                top : (pos.top + 3  ) + "px"
-            }).show();
-            
-            var newScore = document.getElementById("numberCircle").innerHTML;
-            var value = parseInt(newScore,10) + 1;
-            document.getElementById("numberCircle").innerHTML = value;
-            
-            break;
-        }
+    var newDiv = document.createElement("div");
+    newDiv.id = newDivId
+    newDiv.className = "numberCircle";
+    newDiv.innerHTML = value;
+    document.getElementById("circleContent").appendChild(newDiv);
+
+    circlePosition(event, newDivId, pos)
+}
+
+function circlePosition(event, id, position) {
+    var circle = document.querySelector("#" + id);
+    var imgWidth = document.getElementsByClassName("img")[0].offsetWidth;
+    debugger
+    var x = event.clientX - (circle.offsetWidth / 2) - ((imgWidth - 5) * 2.2);
+    var y = event.clientY - (circle.offsetWidth / 2) - imgWidth + 25;
+    var translate3dValue = "translate3d(" + x + "px," + y + "px,0)";
+    circle.style.transform = translate3dValue;
+
+    if (position) {
+        $("#" + id).css({
+            top: (position.top + 3) + "px"
+        }).show();
     }
 }
+
 
